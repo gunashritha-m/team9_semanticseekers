a6a606f9dcefc018590912985e40180be9bae17d
gunashritha-m <164651823+gunashritha-m@users.noreply.github.com>
2024-12-10 18:15:58 +0530

    Update part1.py

--- part1.py
+++ part1.py
@@ -19,26 +19,26 @@ df.sample(10)
 
 df.info()
 
-# missing values
+
 df.isnull().sum()
 
 # duplicate rows
 df.duplicated().sum()
 
-# Distribution of duplicate and non-duplicate questions
+
 
 print(df['is_duplicate'].value_counts())
 print((df['is_duplicate'].value_counts()/df['is_duplicate'].count())*100)
 df['is_duplicate'].value_counts().plot(kind='bar')
 
-# Repeated questions
+
 
 qid = pd.Series(df['qid1'].tolist() + df['qid2'].tolist())
 print('Number of unique questions',np.unique(qid).shape[0])
 x = qid.value_counts()>1
 print('Number of questions getting repeated',x[x].shape[0])
 
-# Repeated questions histogram
+
 
 plt.hist(qid.value_counts().values,bins=160)
 plt.yscale('log')
